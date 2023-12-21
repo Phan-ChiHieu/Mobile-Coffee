640fc7a19da35910ba2a8f33f80d4b5c8c219835
EdricPhan <edricphan.dev@gmail.com>
2023-12-21 17:07:27 +0700

    udpate code

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import CustomIcon from '../components/CustomIcon';
 
 const HomeScreen = () => {
   return (
     <View>
       <Text>HomeScreen</Text>
+      <CustomIcon name="home" size={25} />
     </View>
   );
 };
